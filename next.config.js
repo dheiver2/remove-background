0fcd975ba9b95412d105f51704f1122c721e2dca
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-01-12 18:49:41 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // Habilita o App Router
-  },
   images: {
     domains: [], // Adicione domínios de imagens externas, se necessário
   },
